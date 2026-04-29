6e8541421a1efddc947c49ab9b4727f3410d8503
Guilherme André Welter <guilherme.a.welter@gmail.com>
2026-04-29 17:01:51 -0300

    Add and build vertical scrolling background

--- src/main.c
+++ src/main.c
@@ -4,11 +4,34 @@
  * Use "Genesis Code: Compile" command to compile this program.
  **/
 #include <genesis.h>
+#include <resources.h>
 
 int main() {
-  VDP_drawText("Hello Sega!!", 10, 13);
+  VDP_loadTileSet(background.tileset, 1, DMA);
+  PAL_setPalette(PAL1, background.palette->data, DMA);
+
+  int i = 0;
+  int thex = 0;
+  int they = 0;
+  int val = 1;
+
+  SYS_disableInts();
+  for (i = 0; i < 1280; i++) {
+    thex = i % 40;
+    they = i / 40;
+    val = (random() % (10 - 1 + 1)) + 1;
+    if (val > 3) val = 1;
+    VDP_setTileMapXY(BG_B, TILE_ATTR_FULL(PAL1, 0, FALSE, FALSE, val), thex, they);
+  }
+
+  int offset = 0;
+  VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);
+  SYS_enableInts();
+
   while (1) {
-    // For versions prior to SGDK 1.60 use VDP_waitVSync instead.
+    VDP_setVerticalScroll(BG_B, offset -= 2);
+    if (offset <= -256) offset = 0;
+
     SYS_doVBlankProcess();
   }
   return (0);
