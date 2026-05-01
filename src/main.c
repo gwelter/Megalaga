aab3b604ac0aedd41a324d081e691bd9c99a09fa
Guilherme André Welter <guilherme.a.welter@gmail.com>
2026-05-01 20:39:11 -0300

    Create enemy with palette change

--- src/main.c
+++ src/main.c
@@ -23,11 +23,12 @@ typedef struct {
 Entity enemies[MAX_ENEMIES];
 u16 enemies_left = 0;
 
+int i;
+
 void init_background(void) {
   VDP_loadTileSet(background.tileset, 1, DMA);
   PAL_setPalette(PAL1, background.palette->data, DMA);
 
-  int i;
   int the_x;
   int the_y;
   int value;
@@ -57,10 +58,7 @@ void revive_entity(Entity* entity) {
   SPR_setVisibility(entity->sprite, VISIBLE);
 }
 
-int main(void) {
-  init_background();
-  SPR_init();
-
+void create_player() {
   Entity player_entity = {
       .x = 152,
       .y = 192,
@@ -75,6 +73,33 @@ int main(void) {
 
   player_entity.sprite = SPR_addSprite(&ship, player_entity.x, player_entity.y, TILE_ATTR(PAL1, 0, FALSE, FALSE));
   // SPR_setAnim(player_entity.sprite, 0);
+}
+
+void create_enemies() {
+  PAL_setPalette(PAL2, background.palette->data, DMA);
+  Entity* e = enemies;
+  for (i = 0; i < MAX_ENEMIES; i++) {
+    e->x = i * 32;
+    e->y = 32;
+    // Enemey prite is 2 by 2 meaning 8+8 by 8+8 (default sprite size)
+    e->w = 16;
+    e->h = 16;
+    e->vel_x = 1;
+    e->health = 1;
+    e->sprite = SPR_addSprite(&ship, e->x, e->y, TILE_ATTR(PAL2, 0, TRUE, FALSE));
+    sprintf(e->name, "En%d", i);
+    enemies_left++;
+    e++;
+  }
+  PAL_setColor(34, RGB24_TO_VDPCOLOR(0x0078f8));
+}
+
+int main() {
+  SPR_init();
+  init_background();
+
+  create_player();
+  create_enemies();
   SPR_update();
 
   int offset = 0;
